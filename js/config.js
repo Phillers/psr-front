30a80578a3d57831b57862d90a494a567684dea1
Filip <phillers95@gmail.com>
2019-05-26 20:39:55 +0200

    config

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-central-1_kBIbH1bCB', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6vhhpkdqg8inu75dialhhki28o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://hrnhk4gu3h.execute-api.eu-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
